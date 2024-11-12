a37065d58c950896fd010ca4f19dc4504c03dda1
DarksRoth <70455929+DarksRoth@users.noreply.github.com>
2024-11-11 23:25:13 -0300

    .

--- app/servicios.php
+++ app/servicios.php
@@ -4,14 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style-servicios.css">
+    <link rel="stylesheet" href="../style/style-servicios.css">
     <title>Servicios</title>
 </head>
 
 <body>
 
-<div class="servicios">
-
+<div class="caja-servicios">
+    <div class="servicios">
+        <h1 class="titulo">Servicios</h1>
+        <hr>
+        <table>
+            <tr>N° Servicio</tr>
+            <tr>Fecha_Servicio</tr>
+            <tr>Kilometraje</tr>
+            <tr>Observaciones</tr>
+    </div>
 </div>
 
     <?php
